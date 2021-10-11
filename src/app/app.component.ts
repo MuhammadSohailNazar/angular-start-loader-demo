26dffedeebd0ca449636be21722a6614e92fbb6f
Muhammad Sohail Nazar <sohail@tcig.co>
2021-10-11 22:28:18 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'msn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-start-loader-demo';
+}
